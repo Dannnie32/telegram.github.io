e0354a8f3a057443c72de5b61f09dfe7b46363e6
Dannnie32 <dannnie55@gmail.com>
2025-10-16 08:30:19 +0300

    login_example.php

--- login_example.php
+++ login_example.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BOT_USERNAME', 'XXXXXXXXXX'); // place username of your bot here
+define('@main_main7_bot', '8447111888:AAFfpo53zLFXcdhqrZm09LoOWOciJubzMak'); // place username of your bot here
 
 function getTelegramUserData() {
   if (isset($_COOKIE['tg_user'])) {
@@ -32,7 +32,7 @@ if ($tg_user !== false) {
   }
   $html .= "<p><a href=\"?logout=1\">Log out</a></p>";
 } else {
-  $bot_username = BOT_USERNAME;
+  $bot_username = @main_main7_bot;
   $html = <<<HTML
 <h1>Hello, anonymous!</h1>
 <script async src="https://telegram.org/js/telegram-widget.js?2" data-telegram-login="{$bot_username}" data-size="large" data-auth-url="check_authorization.php"></script>
